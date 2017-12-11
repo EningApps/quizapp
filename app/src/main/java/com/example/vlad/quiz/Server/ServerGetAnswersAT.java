2d61155312ecec2eb0751d4f59af29d3269a08f4
Ening <eningapps@gmail.com>
2017-12-11 18:30:20 +0300

    first commit

---
+++ app/src/main/java/com/example/vlad/quiz/Server/ServerGetAnswersAT.java
@@ -0,0 +1,46 @@
+package com.example.vlad.quiz.Server;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.vlad.quiz.Quiz;
+
+import java.io.BufferedInputStream;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URLConnection;
+
+/**
+ * Created by User on 10.12.2017.
+ */
+
+public class ServerGetAnswersAT extends AsyncTask<Void ,Void ,String>{
+
+    private static final String URLadress="https://quizapplab.herokuapp.com/api/";
+    private Quiz quiz;
+    public ServerGetAnswersAT(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try
+        {
+            URL url = new URL (URLadress+"answers/");
+            URLConnection conn = url.openConnection();
+            BufferedReader reader=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String s=reader.readLine();
+            Log.d("MYTAG", s);
+            this.quiz.setRowAnswers(s.split(" "));
+            this.quiz.setRowQuestions(s.split(" "));
+            return s;
+        }
+        catch (IOException e) {
+        }
+        return "problebDoInBackAnswers";
+    }
+
+}
